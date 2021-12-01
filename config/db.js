6551618b0ab718b49255ae7965053dc06d98fb86
hironomiu <hironomiu@gmail.com>
2021-12-01 06:40:55 +0000

    commited

--- config/db.js
+++ config/db.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2')
-
+require('dotenv').config()
 const pool = mysql.createPool({
   connectionLimit: 5,
-  host: 'db',
-  user: 'appuser',
-  password: 'mysql',
-  database: 'test',
+  host: process.env.DB_HOST || 'db',
+  user: process.env.DB_USERNAME || 'appuser',
+  password: process.env.DB_PASSWORD || 'mysql',
+  database: process.env.DB_NAME || 'test',
+  port: process.env.DB_PORT || 3306
 })
 
 module.exports = promisePool = pool.promise()
